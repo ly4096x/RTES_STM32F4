4f4a6bd8d57aad4c48113e9b4422f17d7ed0999f
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2019-12-11 19:22:13 -0500

    shell working now. Solved thread multiple notification problem. (hopefully) see "ThreadUtils.cc : 12"

--- lib/xHAL/ThreadUtils.cc
+++ lib/xHAL/ThreadUtils.cc
@@ -3,14 +3,33 @@
 namespace xHAL {
 
 void waitForNotification(const TaskNotificationIds expectedNotifyId, u32 deadline, bool waitForever) {
+#if 1
     u32 notifiedValue = INVALID_NOTIFY_VALUE;
     while (notifiedValue != expectedNotifyId) {
         if (!xTaskNotifyWait(0, 0, &notifiedValue, waitForever ? portMAX_DELAY : getTimeout(deadline)))
             FailAndInfiniteLoop();
     }
+#else // disabled since this workaround (setting caller to NULL after notifying) seems pretty good.
+    // This is trying to solve one task being notified multiple times and all previous unmatching notifications got lost
+    // only partly solved since the following implementation allow only one unmatching notification to be forwarded
+    u32 notifiedValue = INVALID_NOTIFY_VALUE;
+    u32 pending = INVALID_NOTIFY_VALUE;
+    while (notifiedValue != expectedNotifyId) {
+        if (!xTaskNotifyWait(0, 0, &notifiedValue, waitForever ? portMAX_DELAY : getTimeout(deadline)))
+            FailAndInfiniteLoop();
+        if (notifiedValue != expectedNotifyId) {
+            debug_break();
+            pending = notifiedValue;
+        }
+    }
+    auto caller = xTaskGetCurrentTaskHandle();
+    if (pending != INVALID_NOTIFY_VALUE)
+        notifyThread(caller, (TaskNotificationIds)pending);
+#endif
 }
 
-void notifyThread(const TaskHandle_t &caller, const TaskNotificationIds notifyId) {
+void notifyThread(TaskHandle_t &caller, const TaskNotificationIds notifyId) {
+    if (!caller) return;
     if (isInISR()) {
         BaseType_t shouldYield;
         xTaskNotifyFromISR(caller, notifyId, eSetValueWithOverwrite, &shouldYield);
@@ -18,6 +37,7 @@ void notifyThread(const TaskHandle_t &caller, const TaskNotificationIds notifyId
     } else {
         xTaskNotify(caller, notifyId, eSetValueWithOverwrite);
     }
+    caller = nullptr;
 }
 
 }
